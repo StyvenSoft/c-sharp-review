ac13cc79a37bda387299221e59e99e0a4b2fd3a0
Steveen <steveen.echeverri@gmail.com>
2020-10-15 17:14:52 -0500

    Completed exercise Console

---
+++ ConsoleCreatures/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o o)");    
+            Console.WriteLine("| O |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("c_     ___");
+            Console.WriteLine("  \\\\__|__|__");
+            Console.WriteLine("　　 \\( o_o)");
+            Console.WriteLine("　　　 > ~  >");
+            Console.WriteLine("　　　/ 　 / \\\\");
+            Console.WriteLine("　　 /　　/　 \\\\");
+            Console.WriteLine("　　 \\　 )　　c/");
+            Console.WriteLine("　　/　 /");
+            Console.WriteLine("　 /　/|");
+            Console.WriteLine("　(　( \\ ");
+            Console.WriteLine("　|　|  \\ ");
+            Console.WriteLine("　| / \\ )");
+            Console.WriteLine("　| |　) |");
+            Console.WriteLine(" /  )(_/");
+            Console.WriteLine("(_ /");
+            
+        }
+    }
+}
