4360d433ad7b8170e4da3d66fafc322dfe7bf114
Steveen <steveen.echeverri@gmail.com>
2021-04-07 21:43:26 -0500

    Exercise basic averageof array

---
+++ AverageOfanArray/AverageOfanArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageOfanArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 63, 78, 22, 98, 52, 74, 32 };
+
+            int sum = 0, avg, i;
+
+            for (i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            avg = sum / i;
+
+            Console.WriteLine("The average is: " + avg);
+            Console.ReadKey();
+        }
+    }
+}
