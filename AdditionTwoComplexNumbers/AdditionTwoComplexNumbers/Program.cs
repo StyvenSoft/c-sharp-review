bcf5ebbdb2aabf6ab72e3db5e7a9c081f09227ef
Steveen <steveen.echeverri@gmail.com>
2021-04-07 22:34:24 -0500

    Example addition two complex numbers

---
+++ AdditionTwoComplexNumbers/AdditionTwoComplexNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionTwoComplexNumbers
+{
+    class MyComplex
+    {
+        int r, i;
+
+        public MyComplex(int r, int i)
+        {
+            this.r = r;
+            this.i = i;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(r + "+" + i + "i");
+            Console.ReadKey();
+        }
+
+        public static MyComplex operator + (MyComplex Comp1, MyComplex Comp2)
+        {
+            MyComplex Comp3 = new MyComplex(Comp1.r + Comp2.r, Comp1.i + Comp2.i);
+
+            return Comp3;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplex C1 = new MyComplex(5, 4);
+            MyComplex C2 = new MyComplex(10, 3);
+            MyComplex C3 = C1 + C2;
+            Console.WriteLine("The addition of the two complex numbers is: ");
+            C3.show();
+        }
+    }
+}
---
+++ AdditionTwoComplexNumbers/AdditionTwoComplexNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("AdditionTwoComplexNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyProduct("AdditionTwoComplexNumbers")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("c91ba3c7-a2f1-4bc1-ab14-0321b9f96a59")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
