f4ea8bb195960b169aaf0c1ec2ecbdbceec1872f
Steveen <steveen.echeverri@gmail.com>
2021-04-07 21:59:03 -0500

    Example reverse of a number basic

---
+++ reverseOfaNumber/reverseOfaNumber/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverseOfaNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, reverse;
+
+            Console.WriteLine("Enter a Number: ");
+
+            num = Convert.ToInt32(Console.ReadLine());
+
+            while (num != 0)
+            {
+                reverse = num % 10;
+
+                num = num / 10;
+                Console.Write(reverse);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
