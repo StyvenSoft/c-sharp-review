7c24e55ed4c1978745ed3d916b8b1e4a2a5feec0
Steveen <steveen.echeverri@gmail.com>
2021-04-07 21:26:44 -0500

    Created project from VS Sum of array

---
+++ SumOfanArray/SumOfanArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfanArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 15, 65, 56, 85, 63, 45, 22, 84, 65, 42, 88, 54};
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+
+            Console.WriteLine("The sum of the elements is: " + sum);
+            Console.ReadKey();
+
+        }
+    }
+}
